06addafa81fe335effc074b50fb2863ec24de328
Ramon <Rahmon@users.noreply.github.com>
2017-06-08 16:17:48 -0300

    Remove unused functions

--- includes/class-wc-product-page-customize.php
+++ includes/class-wc-product-page-customize.php
@@ -167,8 +167,6 @@ class Wc_Product_Page_Customize {
 
 		$plugin_public = new Wc_Product_Page_Customize_Public( $this->get_plugin_name(), $this->get_version() );
 
-		$this->loader->add_action( 'wp_enqueue_scripts', $plugin_public, 'enqueue_styles' );
-		$this->loader->add_action( 'wp_enqueue_scripts', $plugin_public, 'enqueue_scripts' );
 		$this->loader->add_action( 'woocommerce_before_single_product', $plugin_public, 'customize_product_page' );
 
 	}
--- public/class-wc-product-page-customize-public.php
+++ public/class-wc-product-page-customize-public.php
@@ -54,52 +54,6 @@ class Wc_Product_Page_Customize_Public {
 
 	}
 
-	/**
-	 * Register the stylesheets for the public-facing side of the site.
-	 *
-	 * @since    1.0.0
-	 */
-	public function enqueue_styles() {
-
-		/**
-		 * This function is provided for demonstration purposes only.
-		 *
-		 * An instance of this class should be passed to the run() function
-		 * defined in Wc_Product_Page_Customize_Loader as all of the hooks are defined
-		 * in that particular class.
-		 *
-		 * The Wc_Product_Page_Customize_Loader will then create the relationship
-		 * between the defined hooks and the functions defined in this
-		 * class.
-		 */
-
-		wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'css/wc-product-page-customize-public.css', array(), $this->version, 'all' );
-
-	}
-
-	/**
-	 * Register the JavaScript for the public-facing side of the site.
-	 *
-	 * @since    1.0.0
-	 */
-	public function enqueue_scripts() {
-
-		/**
-		 * This function is provided for demonstration purposes only.
-		 *
-		 * An instance of this class should be passed to the run() function
-		 * defined in Wc_Product_Page_Customize_Loader as all of the hooks are defined
-		 * in that particular class.
-		 *
-		 * The Wc_Product_Page_Customize_Loader will then create the relationship
-		 * between the defined hooks and the functions defined in this
-		 * class.
-		 */
-
-		wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/wc-product-page-customize-public.js', array( 'jquery' ), $this->version, false );
-
-	}
-
 	public function customize_product_page() {
 		/**
 		 * woocommerce_single_product_summary hook.
