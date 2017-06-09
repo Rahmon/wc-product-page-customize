d0c9d18a584ee6e400dd5c3771085b9cb8826bde
Ramon <Rahmon@users.noreply.github.com>
2017-06-09 15:59:47 -0300

    Add settings page display

--- admin/partials/wc-product-page-customize-admin-display.php
+++ admin/partials/wc-product-page-customize-admin-display.php
@@ -11,6 +11,58 @@
  * @package    Wc_Product_Page_Customize
  * @subpackage Wc_Product_Page_Customize/admin/partials
  */
+
+ /**
+  * woocommerce_single_product_summary hook.
+  *
+  * @hooked woocommerce_template_single_title - 5
+  * @hooked woocommerce_template_single_rating - 10
+  * @hooked woocommerce_template_single_price - 10
+  * @hooked woocommerce_template_single_excerpt - 20
+  * @hooked woocommerce_template_single_add_to_cart - 30
+  * @hooked woocommerce_template_single_meta - 40
+  * @hooked woocommerce_template_single_sharing - 50
+  * @hooked WC_Structured_Data::generate_product_data() - 60
+  */
 ?>
 
-<!-- This file should primarily consist of HTML with a little bit of PHP. -->
+<h1>WC Product Page Customize</h1>
+<form>
+  <div>
+    <span>Title</span>
+    <input type="number" value="5"/>
+  </div>
+
+  <div>
+    <span>Rating</span>
+    <input type="number" value="10"/>
+  </div>
+
+  <div>
+    <span>Price</span>
+    <input type="number" value="10"/>
+  </div>
+
+  <div>
+    <span>Excerpt</span>
+    <input type="number" value="20"/>
+  </div>
+
+  <div>
+    <span>Add to Cart</span>
+    <input type="number" value="30"/>
+  </div>
+
+  <div>
+    <span>Meta</span>
+    <input type="number" value="40"/>
+  </div>
+
+  <div>
+    <span>Sharing</span>
+    <input type="number" value="50"/>
+  </div>
+  <div>
+    <input type="submit" value="Save" />
+  </div>
+</form>
